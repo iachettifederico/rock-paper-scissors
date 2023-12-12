987c16b576bb443cbb5bc0961ba26bb78283b7bd
Federico Iachetti <iachetti.federico@gmail.com>
2023-12-12 17:22:49 -0300

    Refactored

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,9 @@
 
 class Game
   def self.with(player1:, player2:)
+    raise INVALID_CHOICE unless valid_player?(player1)
+    raise INVALID_CHOICE unless valid_player?(player2)
+
     new(player1: player1, player2: player2)
   end
 
@@ -25,5 +28,15 @@ class Game
   def initialize(player1:, player2:)
     @player1 = player1
     @player2 = player2
+  end
+
+  INVALID_CHOICE = "Invalid choice"
+
+  def self.available_choices
+    %i[rock paper scissors]
+  end
+
+  def self.valid_player?(player)
+    available_choices.include?(player)
   end
 end
--- spec/suite/a_spec.rb
+++ spec/suite/a_spec.rb
@@ -11,6 +11,22 @@ RSpec.describe "My spec" do
     )
   }
 
+  describe "invalid choices" do
+    context "player1 is invalid" do
+      Given(:player1) { :non_valid_choice }
+      Given(:player2) { :rock }
+
+      Then { game == Failure(RuntimeError, Game::INVALID_CHOICE) }
+    end
+
+    context "player2 is invalid" do
+      Given(:player1) { :paper }
+      Given(:player2) { :non_valid_choice }
+
+      Then { game == Failure(RuntimeError, Game::INVALID_CHOICE) }
+    end
+  end
+
   describe "player1 wins rock vs scissors" do
     Given(:player1) { :rock }
     Given(:player2) { :scissors }
